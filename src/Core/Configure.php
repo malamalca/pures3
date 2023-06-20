79172394dfd0ad2ec1ba5b3cc4b1b23b2f82376e
Miha Nahtigal <miha.nahtigal@gmail.com>
2023-06-20 19:59:30 +0200

    Stan fixes

--- src/Core/Configure.php
+++ src/Core/Configure.php
@@ -55,7 +55,7 @@ class Configure
         $ret = $default;
         $levels = (array)explode('.', $key);
 
-        if (!empty($levels)) {
+        if (count($levels) > 0) {
             $base = $instance->config;
 
             $i = 0;
